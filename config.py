380ed4db3d5c3324107efc9a4fb3a04aaf8e7052
Catalina San Juan <catalina.sanjuan@inacapmail.cl>
2025-01-27 15:49:29 -0300

    db.py

--- config.py
+++ config.py
@@ -1,9 +1,18 @@
+import os
+from urllib.parse import urlparse
 
+# Obtén la URL de la base de datos desde las variables de entorno
+database_url = os.getenv('MYSQL_URL')
 
-db_config = {
-    'user': parsed_url.username,
-    'password': parsed_url.password,
-    'host': parsed_url.hostname,
-    'database': parsed_url.path[1:],  # Elimina el primer "/"
-    'port': parsed_url.port
-}
+# Analiza la URL
+if database_url:
+    parsed_url = urlparse(database_url)
+    db_config = {
+        'user': parsed_url.username,
+        'password': parsed_url.password,
+        'host': parsed_url.hostname,
+        'port': parsed_url.port,
+        'database': parsed_url.path.lstrip('/'),
+    }
+else:
+    raise ValueError("MYSQL_URL no está definida en las variables de entorno.")
