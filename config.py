a0fdd8ba497de42baebf47dccd4191e99b8685d1
Catalina San Juan <catalina.sanjuan@inacapmail.cl>
2025-01-29 12:39:38 -0300

    update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@
 import os
 
 db_config = {
-    'user': os.getenv('DB_USER'),
-    'password': os.getenv('DB_PASSWORD'),
-    'host': os.getenv('DB_HOST'),
-    'database': os.getenv('DB_NAME')
+    'user': os.getenv('DB_USER', '396794'),  # Usuario de AlwaysData
+    'password': os.getenv('DB_PASSWORD', 'admin2025'),  # Contraseña de AlwaysData
+    'host': os.getenv('DB_HOST', 'mysql-libreria-el-gran-poeta.alwaysdata.net'),  # Servidor de AlwaysData
+    'database': os.getenv('DB_NAME', 'libreria-el-gran-poeta_bd')  # Base de datos de AlwaysData
 }
